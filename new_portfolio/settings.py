4f5f6bd1186fdf21b752b49ba0fa68e6a546ebf2
sooradje <soo.mauve.pro@gmail.com>
2024-10-07 11:43:04 +0200

    add host in settings.py

--- new_portfolio/settings.py
+++ new_portfolio/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','votre-futur-alternant.onrender.com']
 
 
 # Application definition
